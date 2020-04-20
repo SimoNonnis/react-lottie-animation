8369fcdffa56e339bddb5f4a1b30d7f02e83464b
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-04-20 17:40:54 +0100

    Options outside component

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -3,16 +3,16 @@ import Lottie from 'react-lottie';
 
 import animationDrink from '../animations/animationDrink.json';
 
-const Drink = () => {
-  const options = {
-    loop: true,
-    autoplay: true,
-    animationData: animationDrink,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  };
+const options = {
+  loop: true,
+  autoplay: true,
+  animationData: animationDrink,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
 
+const Drink = () => {
   return <Lottie options={options} height={300} width={300} />;
 };
 
