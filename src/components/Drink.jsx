e8f81c4207049a9cf7735d05a07fe6dc05d80b96
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2020-04-20 17:58:57 +0100

    Add state

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lottie from 'react-lottie';
 
 import animationDrink from '../animations/animationDrink.json';
@@ -13,7 +13,16 @@ const options = {
 };
 
 const Drink = () => {
-  return <Lottie options={options} height={300} width={300} />;
+  const [paused, setPaused] = useState(false);
+
+  const handlePause = () => setPaused(!paused);
+
+  return (
+    <div>
+      <Lottie options={options} height={300} width={300} isPaused={paused} />
+      <button onClick={handlePause}>{paused ? 'Play' : 'Pause'}</button>
+    </div>
+  );
 };
 
 export default Drink;
